76efc4e3a89f8ae4ff4b16ef7f256e19e3e4762f
Quetzal <quetzaluzcategui@gmail.com>
2024-06-02 22:54:49 -0400

    Initial Commit

---
+++ I'm NOT rich/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  I'm NOT rich
+//
+//  Created by Quetzal on 2/6/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
